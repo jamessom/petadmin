846ffbb8a10ceb3339f1af24535059cec99e5f4f
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2020-04-26 16:11:53 -0300

    Fix mail catcher port

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,6 +20,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => 'mailcatcher', :port => 1025 }
+  config.action_mailer.smtp_settings = { :address => 'mailcatcher', :port => 25 }
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
   config.assets.debug = true
