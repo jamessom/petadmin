3936e7bb381a3b7eb5a0904ddffd8a8977e335d3
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2020-04-26 00:18:12 -0300

    Update models to add dependent destroy With this feature it's possible to delete data from relationship tables

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,15 +1,16 @@
 class Client < ApplicationRecord
   include Fae::BaseModelConcern
-
+  
   validates :phone, presence: true
   validates :email, uniqueness: true, presence: true, format: {
     with: Fae.validation_helpers.email_regex,
     message: 'You need use a valid and unique email'
   }
+  
+  has_many :campaign_clients, dependent: :destroy
+  has_many :campaigns, through: :campaign_clients
 
   def fae_display_field
     name
   end
-
-
 end
